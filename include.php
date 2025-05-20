633d3bfad8a2c121e28ef29c211fad08e5195446
Katya <kalapasa@bk.ru>
2025-05-20 13:58:49 +0300

    Добавлен пояснительный комментарий

--- include.php
+++ include.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php'; //autoload нужен, когда происходит обращение из одного namespace к методу класса в другом namespace
+require_once __DIR__ . '/vendor/autoload.php'; //autoload нужен, когда происходит обращение из одного namespace к методу класса в другом namespace, чтобы можно было use-ать в общем что тут непонятного-то, м?
 
 
 

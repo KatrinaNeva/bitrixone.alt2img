11553945e4414c5ab6655e7306fead2401c965d2
Katya <kalapasa@bk.ru>
2025-05-20 13:54:23 +0300

    Добавлена новая строка

--- lib/TestElse.php
+++ lib/TestElse.php
@@ -3,7 +3,8 @@ namespace Bitrixone\Alt2Img;
 
 class TestElse {
     public static function showSomeThingElse () {
-        echo 'Сам гавно!';
+        echo 'Сам ты такой, нехороший человечек!<br>';
+        echo 'А шо такое?';
         $obj = new Test();
         $obj->showSomething();
         Test::showSomething();
